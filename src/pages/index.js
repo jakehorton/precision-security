2ea67952ac0d4862b5fb135141e9b4526247438c
Jake Horton <jake@studiomade.co>
2020-12-21 10:55:10 +0000

    github test

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hi </h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </>
